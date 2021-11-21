2926206708081c47e24228e8f4d8843481bbd1a7
AvramAndrei-Alexandru <57368962+AvramAndrei-Alexandru@users.noreply.github.com>
2021-11-21 14:59:38 +0200

    Changed base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //baseUrl: "https://energy-platform-backend.herokuapp.com/api/"
-  baseUrl: "https://localhost:44353/api/"
+  baseUrl: "https://energy-platform-backend.herokuapp.com/api/"
+  //baseUrl: "https://localhost:44353/api/"
 };
  
